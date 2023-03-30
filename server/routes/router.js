9644860e3c9ee8404a5ae164f936a3db6030579e
Hogale <262164@NTTDATA.COM>
2023-03-30 13:04:26 +0530

    delete button added

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,72 +6,92 @@ const moment = require("moment")
 
 //image stotage confige
 var imgconfig = multer.diskStorage({
-    destination:(req,file,callback)=>{
-        callback(null,"./uploads")
+    destination: (req, file, callback) => {
+        callback(null, "./uploads")
     },
-    filename:(req,file,callback)=>{
-        callback(null,`image-${Date.now()}.${file.originalname}`)
+    filename: (req, file, callback) => {
+        callback(null, `image-${Date.now()}.${file.originalname}`)
     }
 });
 
-const isImage = (req,file,callback)=>{
-    if(file.mimetype.startsWith("image")){
-        callback(null,true)
+const isImage = (req, file, callback) => {
+    if (file.mimetype.startsWith("image")) {
+        callback(null, true)
     }
-    else{
-        callback(null,Error("only image is allowed"));
+    else {
+        callback(null, Error("only image is allowed"));
     }
 }
 
 var upload = multer({
-    storage:imgconfig,
-    fileFilter:isImage
+    storage: imgconfig,
+    fileFilter: isImage
 })
 
 //register userdata
-router.post("/register",upload.single("photo"),(req,res)=>{
-// console.log(req.body)
-// const {fname} = req.body;
-// const {filename} = req.file;
-// const  {price} = req.price;
-// const {description} = req.description;
-const {fname, description, price} = req.body;
-const {filename} = req.file;
+router.post("/register", upload.single("photo"), (req, res) => {
+    // console.log(req.body)
+    // const {fname} = req.body;
+    // const {filename} = req.file;
+    // const  {price} = req.price;
+    // const {description} = req.description;
+    const { fname, description, price } = req.body;
+    const { filename } = req.file;
 
-console.log("data");
-if(!fname || !filename || !price || !description){
-    res.status(422).json({status:422,message:"fil all the details"});
-}
-    try{
-// let date = moment(new Date()).format("YYYY-MM-DD")
-conn.query("INSERT INTO productlist (fname, description, price, photo) VALUES ($1, $2, $3, $4)", [fname, description, price, filename], (err,result) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log("data added");
-        res.status(201).json({status:201,data:req.body})
+    console.log("data");
+    if (!fname || !filename || !price || !description) {
+        res.status(422).json({ status: 422, message: "fil all the details" });
     }
-})
-    }catch(error){
-        res.status(422).json({status:422,error});
+    try {
+        // let date = moment(new Date()).format("YYYY-MM-DD")
+        conn.query("INSERT INTO productlist (fname, description, price, photo) VALUES ($1, $2, $3, $4)", [fname, description, price, filename], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("data added");
+                res.status(201).json({ status: 201, data: req.body })
+            }
+        })
+    } catch (error) {
+        res.status(422).json({ status: 422, error });
     }
 })
 
 //get user data
-router.get("/getdata",(req,res)=>{
-    try{
-conn.query("SELECT * FROM productlist",(err,result)=>{
-    if(err){
-        console.log("error");
-    }else{
-        console.log("data get");
-        res.status(201).json({status:201,data:result})
-        // console.log(data)
-        // console.log(result)
-    }
-});
-    }catch(error){
-        res.status(422).json({status:422,error})
+router.get("/getdata", (req, res) => {
+    try {
+        conn.query("SELECT * FROM productlist", (err, result) => {
+            if (err) {
+                console.log("error");
+            } else {
+                console.log("data get");
+                res.status(201).json({ status: 201, data: result })
+                // console.log(data)
+                // console.log(result)
+            }
+        });
+    } catch (error) {
+        res.status(422).json({ status: 422, error })
     }
 })
+
+
+//delete product
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    try {
+        conn.query(`DELETE FROM productlist WHERE id ='${id}'`, (err, result) => {
+            if (err) {
+                console.log("error")
+            } else {
+                console.log("data delete")
+                res.status(201).json({ status: 201, data: result })
+            }
+        })
+    } catch (error) {
+        res.status(422).json({ status: 422, error })
+    }
+})
+
+
 module.exports = router;
