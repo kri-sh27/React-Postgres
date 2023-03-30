fec4bb4f617d2b70437365b188176eb58c3ed35a
Hogale <262164@NTTDATA.COM>
2023-03-30 13:03:52 +0530

    delete feature added

--- imageviewer/src/components/Home.js
+++ imageviewer/src/components/Home.js
@@ -1,68 +1,137 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import Button from 'react-bootstrap/Button';
-import { NavLink } from 'react-router-dom';
+
+import { NavLink } from "react-router-dom"
+
 import Card from 'react-bootstrap/Card';
+
 import axios from 'axios';
-import { useEffect ,useState} from 'react'
+
+import moment from "moment";
+
+
+
 
 const Home = () => {
+
+
+
+
     const [data, setData] = useState([]);
 
+
+
+
     const getUserData = async () => {
-        const res = await axios.get("/getdata",{
-            headers:{
+
+        const res = await axios.get("/getdata", {
+
+            headers: {
+
                 "Content-Type": "application/json"
+
             }
+
         });
 
         if (res.data.status == 201) {
+
             console.log("data get");
-            setData(res.data.data)
+
+            setData(res.data.data.rows)
+
+            //console.log(res.data.data)
+
         } else {
+
             console.log("error")
+
         }
-        console.log(res)
+
     }
-    useEffect(()=>{
+
+
+
+
+    const dltUser = async (id) => {
+
+        console.log(id)
+
+        const res = await axios.delete(`/${id}`, {
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            }
+
+        });
+
+        if (res.data.status == 201) {
+
+            getUserData()
+
+
+
+        } else {
+
+            console.log("error")
+
+        }
+
+    }
+
+
+
+
+
+    useEffect(() => {
+
         getUserData()
-    },[])
+
+    }, [])
+
+    console.log('data', data)
+
+    console.log('data', data.length)
+
+    //console.log('length',data.rows.length)
 
     return (
-        <>
-            <div className='container mt-2'>
-                <h1>this is list {data.fname}</h1>
-                <div className='text-end'>
-                    <Button variant="primary"><NavLink to='/register' className="text-decoration-none text-light">Add product</NavLink></Button>{''}
-                </div>
+        <div className="container mt-2">
+            <h1 className='text-center mt-2'>Product list With Postgress database</h1>
+            <div className='text-end'>
+                <Button variant="primary"><NavLink to="/register" className="text-decoration-none text-light"> Add Product</NavLink></Button>
             </div>
-            <div className='d-flex justify-content-between align-items-center mt-5'>
+            <div className='d-flex justify-content-between align-iteams-center mt-5'>
                 {
-                    data.length > 0 ?data.map((el,i)=>{
-                        return(
-                           
-                        <>
-                      < h1> {el}</h1>
-                        {el.fname}
-                    {data.fname}
-                 {/* <Card style={{ width: '15rem', height: '15rem' }} className="mb-3">
-                            <Card.Img variant="top" src="logo192.png" style={{ width: '50px', textAlign: "center", margin: "auto" }} className="mt-2" />
-                            <Card.Body className='text-center'>
-                                <Card.Title>user name : {el.fname}</Card.Title>
-                                {/* <Card.Title>date 42e3556</Card.Title> */}
-        
-                                {/* <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text> */}
-                                {/* <Button variant="primary" className='col-lg-6 text-center'>Delete</Button>
-                            </Card.Body> */}
-                        {/* </Card> */} 
-                        </>
+                    data.length > 0 ? data.map((el, i) => {
+                        return (
+                            <>
+                                <Card style={{ width: '20rem', height: "14rem" }} className="mb-3">
+                                    <Card.Img variant="top" src={`/uploads/${el.photo}`} style={{ width: '100px', height:'100px', textAlign: "center", margin: "auto" }} className="mt-2" />
+                                    <Card.Body className='text-center'>
+                                        <Card.Title>Product Name : {el.fname}</Card.Title>
+                                        <Card.Text>
+                                            Description : {el.description}
+                                        </Card.Text>
+                                        <Card.Text>
+                                            Price : {el.price}
+                                        </Card.Text>
+                                        <Button variant="danger" onClick={() => dltUser(el.id)} className='col-lg-6 text-center'>Delete</Button>
+                                    </Card.Body>
+                                </Card>
+                            </>
                         )
-                        }):""
-                    }
+                    }) : ""
+                }
             </div>
-        </>
+        </div>
     )
 }
+
+
+
+
 export default Home
